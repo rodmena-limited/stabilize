4d01a0e8a3e6b3781841a3c790e94994f685c112
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-12-21 00:11:05 +0000

    Add TaskImplementation, _default_registry to registry

--- src/stabilize/tasks/registry.py
+++ src/stabilize/tasks/registry.py
@@ -5,3 +5,5 @@ from stabilize.tasks.interface import CallableTask, Task
 from stabilize.tasks.result import TaskResult
 logger = logging.getLogger(__name__)
 TaskCallable = Callable[["StageExecution"], TaskResult]
+TaskImplementation = type[Task] | Task | TaskCallable
+_default_registry: TaskRegistry | None = None
