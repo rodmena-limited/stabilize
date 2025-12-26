9f253ac959d542f5160871de3188fb23dcd1a5dd
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-12-26 10:00:19 +0000

    New Python script: __init__

---
+++ src/stabilize/stages/__init__.py
@@ -0,0 +1,11 @@
+"""Stage definition builders for pipeline execution."""
+
+from stabilize.stages.builder import (
+    StageDefinitionBuilder,
+    StageDefinitionBuilderFactory,
+)
+
+__all__ = [
+    "StageDefinitionBuilder",
+    "StageDefinitionBuilderFactory",
+]
