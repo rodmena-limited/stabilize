ec77d150ebcd480e8386c407323b9440e2f35316
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-12-28 00:36:47 +0000

    Add RESERVED_KEYS to shell

--- src/stabilize/tasks/shell.py
+++ src/stabilize/tasks/shell.py
@@ -7,3 +7,18 @@ from typing import TYPE_CHECKING, Any
 from stabilize.tasks.interface import Task
 from stabilize.tasks.result import TaskResult
 logger = logging.getLogger(__name__)
+RESERVED_KEYS = frozenset(
+    {
+        "command",
+        "timeout",
+        "cwd",
+        "env",
+        "shell",
+        "stdin",
+        "max_output_size",
+        "expected_codes",
+        "secrets",
+        "binary",
+        "continue_on_failure",
+    }
+)
